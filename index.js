8d8b864913ead34839f63d99c34b0bd12eecdb68
Josh E <jeaber@gmail.com>
2017-08-02 18:08:07 -0500

    known ip

--- index.js
+++ index.js
@@ -41,31 +41,30 @@ var clientRequests = require('./mongoose/client_requests');
 io.on('connection', function (socket) {
     try {
         var clientIp = socket.request.connection.remoteAddress;
+        console.log(typeof clientIp, clientIp);
         var authenticated = false;
         var credentials = {
             email: '',
             password: ''
         };
-        var orders;
-        var accounts;
-        var positions;
+        var accountSave;
         // {'email','password'}
-        socket.on('auth', function (data, socket, clientIp) {
+        socket.on('auth', function (data) {
             credentials = data;
             authenticated = clientRequests.auth(credentials, socket);
-            clientRequests.accountData(credentials, socket);
+            // clientRequests.accountData(credentials, socket);
         });
 
-        socket.on('accountForm', function (account, clientIp) { clientRequests.accountForm(account); });
-        socket.on('contactForm', function (data, account) { clientRequests.contactForm(data, account); });
-        socket.on('fundingForm', function (data, account) { clientRequests.fundingForm(data, account); });
+        socket.on('accountForm', function (data) { account = data; clientRequests.accountForm(data, socket, clientIp); });
+        socket.on('contactForm', function (data, account) { clientRequests.contactForm(data, account, socket); });
+        socket.on('fundingForm', function (data, account) { clientRequests.fundingForm(data, account, socket); });
 
         socket.on('verifyBank', function (data) { clientRequests.verifyBank(data); }); // {'00.12, '00.22'}
         socket.on('verifyEmail', function (data) { clientRequests.verifyEmail(data); });
 
         socket.on('achDeposit', function (data) { clientRequests.achDeposit(data); });
         socket.on('achWithdrawal', function (data) { clientRequests.achWithdrawal(data); });
-        
+
     } catch (e) {
         console.log(e);
     }
